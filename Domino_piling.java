8101b78750132a4153591da526577f75d35cd936
Sai Jetendra <saijithendra807@gmail.com>
2025-10-08 20:17:19 +0530

    CP.800.Solved

---
+++ Domino_piling.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Domino_piling {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        int m=in.nextInt();
+        int n= in.nextInt();
+        int s=2*1;
+        int DP=(m*n)/s;
+        System.out.println(DP);
+
+    }
+    
+}
