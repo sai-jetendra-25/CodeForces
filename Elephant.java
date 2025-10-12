b7893dfecc87a28883420fa9072eb58a9d090c29
Sai Jetendra <saijithendra807@gmail.com>
2025-10-12 20:36:36 +0530

    CP.800.Math

---
+++ Elephant.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Elephant {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int x=in.nextInt();
+        System.out.println((int)Math.ceil(x/5.0));
+     in.close();   
+    }
+    
+}
